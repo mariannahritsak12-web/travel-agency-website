acafc3f2f1f8a9c76aa6f886ebc4d5905398827a
Marianna <mariannahritsak12@gmail.com>
2026-03-24 13:52:10 +0100

    Fixed the filter logic

--- js/script.js
+++ js/script.js
@@ -26,49 +26,57 @@ const offers = [
         time: "10.08 – 17.08.2025",
         duration: 7,
         startDate: "2025-08-10",
-        travelers: 2
+        travelers: 2,
+        price: 5000,
+        link: "oferta1.html"
     },
     {
         id: 2,
-        title: "Kulturowa Przygoda w Japonii",
-        destination: "Japonia",
-        description: "Poznaj fascynującą kulturę Japonii, od tradycyjnych świątyń po nowoczesne miasta. Idealna oferta dla miłośników historii i kultury.",
-        imageUrl: "photos/japonia.webp",
+        title: "Alpy - Aktywny Wypoczynek w Górach",
+        destination: "Alpy",
+        description: "Poznaj fascynującą kulturę Alp, od tradycyjnych wiosek po nowoczesne ośrodki narciarskie. Idealna oferta dla miłośników historii i kultury.",
+        imageUrl: "photos/alpy.jpg",
         cena: 7000,
         meal: "Śniadanie",
         departure: "Kraków",
-        time: "15.09 – 22.09.2025",
+        time: "03.02 – 10.02.2026",
         duration: 7,
-        startDate: "2025-09-15",
-        travelers: 1
+        startDate: "2026-02-03",
+        travelers: 1,
+        price: 7000,
+        link: "oferta2.html"
     },
     {
         id: 3,
-        title: "Egzotyczna Przygoda w Tajlandii",
-        destination: "Tajlandia",
-        description: "Odkryj egzotyczne plaże, tętniące życiem miasta i bogatą kulturę Tajlandii. Idealna oferta dla poszukiwaczy przygód i miłośników natury.",
-        imageUrl: "photos/tajlandia.webp",
+        title: "Magiczny Maroko - Kulturowa Przygoda",
+        destination: "Maroko",
+        description: "Odkryj egzotyczne plaże, tętniące życiem miasta i bogatą kulturę Maroka. Idealna oferta dla poszukiwaczy przygód i miłośników natury.",
+        imageUrl: "photos/maroko.jpg",
         cena: 4500,
         meal: "All inclusive",
         departure: "Gdańsk",
         time: "20.10 – 27.10.2025",
         duration: 7,
         startDate: "2025-10-20",
-        travelers: 4
+        travelers: 4,
+        price: 4500,
+        link: "oferta3.html"
     },
     {
         id: 4,
-        title: "Europejska Odyseja po Stolicach",
-        destination: "Europa",
-        description: "Odkryj piękno i różnorodność europejskich stolic, od romantycznego Paryża po historyczny Rzym. Idealna oferta dla miłośników kultury i historii.",
-        imageUrl: "photos/europa.webp",
+        title: "Nowy Jork - Weekend w Wielkim Jabłku",
+        destination: "Nowy Jork",
+        description: "Odkryj piękno i różnorodność Nowego Jorku, od ikonicznych wieżowców po tętniące życiem dzielnice. Idealna oferta dla miłośników kultury i historii.",
+        imageUrl: "photos/new_york.jpg",
         cena: 6000,
         meal: "Śniadanie",
         departure: "Wrocław",
-        time: "05.11 – 12.11.2025",
-        duration: 7,
+        time: "05.11 – 10.11.2025",
+        duration: 5,
         startDate: "2025-11-05",
-        travelers: 2
+        travelers: 4,
+        price: 6000,
+        link: "oferta4.html"
     }
 ]
 
@@ -79,6 +87,29 @@ let departureDateInput;
 let durationInput;
 let travelersInput;
 let offersContainer;
+let loginForm;
+let usernameInput;
+let passwordInput;
+let errorMessage;
+
+// Validate login form inputs and provide feedback to the user
+function validateLoginForm() { 
+    error = {
+        isValid: false,
+        message: ""
+    }
+   
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const errorMessage = document.getElementById("error-message");
+
+    if (!usernameInput.value) {
+        error.isValid = false;
+        error.message = "Nazwa użytkownika jest wymagana.";
+        errorMessage.textContent = error.message;
+        return error;
+    }
+}
 
 function handleFormSubmission(event) {
     event.preventDefault();
@@ -115,26 +146,31 @@ function findOfferById(offerId) {
 function renderOfferElement(offerData) {
     // Create an offer card element based on the offer data
     var offerElement = document.createElement("div");
-    offerElement.classList.add("col-md-5", "p-0", "offer-card", "mb-4");
+    offerElement.classList.add("row", "offer-card", "mb-4");
 
-    var offerImage = document.createElement("img");
-    offerImage.src = offerData.imageUrl;
-    offerImage.alt = offerData.destination;
-    offerImage.classList.add("img-fluid", "offer-image");
-
-    var offerContent = document.createElement("div");
-    offerContent.classList.add("offer-content", "p-3");
-
-    var offerTitle = document.createElement("h6");
-    offerTitle.classList.add("mb-1");
-    offerTitle.innerHTML = `<strong>${offerData.title}</strong>`;
-
-    var offerDetails = document.createElement("ul");
-    offerDetails.classList.add("mb-1", "text-muted", "small");
-
-    offerElement.appendChild(offerImage);
-    offerContent.appendChild(offerTitle);
-    offerElement.appendChild(offerContent);
+    offerElement.innerHTML = `
+                <div class="col-md-5 p-0">
+                    <img src="${offerData.imageUrl}" alt="${offerData.destination}" class="img-fluid offer-image" />
+                </div>
+                <div class="col-md-7 p-4">
+                    <div class="d-flex justify-content-between">
+                        <h4 class="title">${offerData.title}</h4>
+                        <span class="rating">Ocena: 4.5/5</span>
+                    </div>
+                    <ul class="mt-3 mb-3 ps-3">
+                        <li class="list-style-room">${offerData.duration} dni / ${offerData.duration - 2} nocy</li>
+                        <li class="list-style-wylot">Wylot z: ${offerData.departure}</li>
+                        <li class="list-style-time">${offerData.time}</li>
+                        <li class="list-style-meal">${offerData.meal}</li>
+                        <li class="lis">Ubezpieczenie</li>
+                    </ul>
+                    <p class="fw-semibold">Résidence Odalys Le Hameau du Mottaret<span class="offer-stars">★★★★</span></p>
+                    <div class="mt-3">
+                        <a href="${offerData.link}"><button class="btn btn-orange">Sprawdz cenę</button></a>
+                        <small class="text-muted ms-2">${offerData.price} zł</small>
+                    </div>
+                </div>
+    `;
 
     return offerElement;
 } 
@@ -161,10 +197,6 @@ function initializeBootstrapComponents() {
 
 }
 
-function toggleReadOnlyMode() { 
-    // 
-}
-
 function setupEventListeners() { 
     searchForm.addEventListener("submit", handleFormSubmission);
 }
@@ -187,11 +219,12 @@ function init() {
         setupEventListeners();
     } 
 
+
+    // Select offers container
+    offersContainer = document.getElementById("offers-container");
+
     if (offersContainer) {
         console.log("You're on the offers page. Offers container found.");
-
-        // Select offers container
-        offersContainer = document.getElementById("offers-container");
 
         // Get query parameters from URL
         const urlParams = new URLSearchParams(window.location.search);
